dacb07237e1355ca06364ba23f98d70bf191a3a5
Kadauchi <kadauchi@gmail.com>
2018-04-29 12:53:59 -0700

    New Version.
    Complete rewrite.

--- background.js
+++ background.js
@@ -1,280 +1,188 @@
-const exchange = {
-  rate: null,
-  
-  check () {
-    tools.xhr({
-      method: `GET`,
-      url: `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%20in%20(%22GBPUSD%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`
-    })
-    .then(exchange.parse)
-    .catch(error => {
-      setTimeout(exchange.check, 1000);
-    });
-  },  
-  
-  parse (result) {
-    const obj = JSON.parse(result);
-    if (obj.query.results.rate.Rate) {
-      exchange.rate = obj.query.results.rate.Rate;
-    }
-  }
-};
+let timeout;
+const notified = [];
 
-const prolific = {
-  checked: null, 
-  timeout: null,
-  studies: {},
-  notified: [],
-  available: [],
-    
-  check () {
-    clearTimeout(prolific.timeout);
-    
-    tools.xhr({
-      method: `GET`,
-      url: `https://www.prolific.ac/studies`
-    })
-    .then(prolific.parse)
-    .catch(error => {
-      prolific.timeout = setTimeout(prolific.check, Number(options.interval) * 1000 * 60);
-    });
-    
-    prolific.checked = new Date().toString();
-  },
-  
-  parse (result) {
-    const doc = document.implementation.createHTMLDocument().documentElement; doc.innerHTML = result;
-    prolific.status = null;
-    prolific.available = [];
-    
-    const studies = doc.querySelectorAll(`.study`);
-    
-    if (studies.length > 0) {
-      let resolves = 0;
-      
-      for (let study of studies) {
-        const id = study.id;
-        prolific.available.push(id);
-      
-        tools.xhr({
-          url: `https://www.prolific.ac/api/studies/${id}`
-        })
-        .then(result => {
-          const obj = JSON.parse(result);
-          prolific.studies[obj.id] = obj;
-    
-          if (prolific.notified.includes(obj.id) === false) {
-            tools.notification({
-              url: `https://www.prolific.ac/studies/${obj.id}`,
-              body: [
-                `Reward: £${(obj.reward / 10000).toFixed(2)} - £${obj.average_reward_per_hour.toFixed(2)}/hr ${exchange.rate ? `• $${(obj.reward / 10000 * exchange.rate).toFixed(2)} - $${(obj.average_reward_per_hour * exchange.rate).toFixed(2)}/hr` : ``}`,
-                `Takes ${obj.average_time_taken} minutes • Allowed ${obj.maximum_allowed_time} minutes`,
-                `Available Places: ${obj.places_remaining}/${obj.total_available_places}`
-              ],
-              title: obj.name
-            });
-      
-            if (options.announce === true) {
-              chrome.tts.speak(`Proliffic Study Found!`, {
-                enqueue: true,
-                voiceName: `Google US English`
-              });
-            }
-      
-            prolific.notified.push(obj.id);
-          }
-                    
-          if (++ resolves === studies.length) {
-            prolific.sendStudies();
-          }
-        })
-        .catch(error => {
-          if (!prolific.notified.includes(id)) {            
-            tools.notification({
-              url: `https://www.prolific.ac/studies/${id}`,
-              body: [
-                `Error retrieving information =(`,
-                `${error}`
-              ],
-              title: `Study Found!`
-            });
-          }
-          
-          if (++ resolves === studies.length) {
-            prolific.sendStudies();
-          }
-        });
-      }
-    }
-    else {
-      prolific.sendStudies();
-    }
-    
-    chrome.browserAction.setBadgeBackgroundColor({
-      color: [255, 0, 0, 255]
-    });
-    chrome.browserAction.setBadgeText({
-      text: prolific.available.length > 0 ? prolific.available.length.toString() : ``
-    });
-    
-    prolific.timeout = setTimeout(prolific.check, Number(options.interval) * 1000 * 60);
-  },
-  
-  sendStudies () {
-    const obj = {
-      studies: {},
-      status: prolific.status,
-      checked: prolific.checked,
-      exchangeRate: exchange.rate
-    };
-    
-    for (let i = 0; i < prolific.available.length; i ++) {
-      const id = prolific.available[i];
-      obj.studies[id] = prolific.studies[id];
-    }
-    
-    chrome.runtime.sendMessage({
-      type: `sendStudies`,
-      message: obj
-    });
-  },
-  
-  pend: null,
-  avail: null,
-  
-  balance () {
-    chrome.runtime.sendMessage({
-      type: `balance`,
-      message: {
-        balance: {
-          pend: prolific.pend,
-          avail: prolific.avail
-        }
+function setBadge(studies) {
+  const count = Object.keys(studies).length;
+  const text = count > 0 ? count.toString() : ``;
+  chrome.browserAction.setBadgeText({ text });
+  chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+}
+
+function setChecked() {
+  chrome.storage.local.set({ checked: new Date().toTimeString() });
+}
+
+function setStudies(studies) {
+  chrome.storage.local.set({ studies });
+}
+
+function getStorage(key) {
+  return new Promise(async (resolve, reject) => {
+    chrome.storage.local.get(key, items => {
+      const value = items[key];
+
+      if (value) {
+        resolve(items[key]);
+      } else {
+        reject(new Error(`No Value for the key: ${key}`));
       }
     });
-    
-    tools.xhr({
-      url: `https://www.prolific.ac/participant/account`
-    })
-    .then(result => {
-      const doc = document.implementation.createHTMLDocument().documentElement; doc.innerHTML = result;
-      
-      const paid = doc.querySelector(`a[href="/participant/account/get-paid"]`);
-      
-      if (paid) {
-        prolific.pend = Number(paid.parentElement.children[1].textContent.replace(/[^0-9.]/g, ``));
-        prolific.avail = Number(paid.parentElement.children[2].textContent.replace(/[^0-9.]/g, ``));
-        
-        chrome.runtime.sendMessage({
-          type: `balance`,
-          message: {
-            balance: {
-              pend: prolific.pend,
-              avail: prolific.avail
-            }
-          }
-        });
-      }
-    })
-    .catch(error => {
-      chrome.runtime.sendMessage({
-        type: `balance`,
-        message: {
-          balance: {
-            pend: prolific.pend,
-            avail: prolific.avail
-          }
+  });
+}
+
+function extractStudies(text) {
+  const doc = new DOMParser().parseFromString(text, `text/html`);
+
+  const studies = [...doc.querySelectorAll(`h3 > a[href^="/studies/"]`)].reduce(
+    (accumulator, element) => {
+      const study = element.closest(`.row`);
+      const props = [...study.querySelectorAll(`li`)].map(el =>
+        el.textContent
+          .split(`:`)[1]
+          .replace(/\s\s+/, ``)
+          .trim()
+      );
+
+      const id = element.pathname.replace(`/studies/`, ``);
+
+      Object.assign(accumulator, {
+        [id]: {
+          href: `https://www.prolific.ac/${element.pathname}`,
+          title: element.textContent,
+          hostedBy: props[0],
+          reward: props[1],
+          avgRewardPerHour: props[2],
+          availablePlaces: props[3],
+          maximumAllowedTime: props[4],
+          avgCompletionTime: props[5]
         }
       });
-    });
-  }
-};
 
-const tools = {
-  xhr (obj) {
-    return new Promise(function (resolve, reject) {
-      const xhr = new XMLHttpRequest();
-      xhr.open(obj.method ? obj.method : `GET`, obj.url);
-      
-      if (obj.timeout) {
-        xhr.timeout = obj.timeout;
-      }
-      
-      if (obj.headers) {
-        for (let key in obj.headers) {
-          xhr.setRequestHeader(key, obj.headers[key]);
-        }
-      }
-      
-      if (obj.responseType) {
-        xhr.responseType = obj.responseType;
-      }
-      
-      let formData = obj.formData ? obj.formData : null;
-      if (formData && typeof formData === `object`) {
-        formData = Object.keys(formData)
-          .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(formData[key])}`)
-          .join(`&`);
-      }
-      
-      xhr.send(formData);
-      
-      xhr.onload = function () {
-        if (this.status === 200) resolve(this.response);
-        else reject(`${this.status} - ${this.statusText}`);
-      };
-      xhr.onerror = function () {
-        reject(`${this.status} - ${this.statusText}`);
-      };
-      xhr.ontimeout = function () {
-        reject(`${this.status} - Timeout`);
-      };
+      return accumulator;
+    },
+    {}
+  );
+  return studies;
+}
+
+function getStudies() {
+  return new Promise(async (resolve, reject) => {
+    const response = await fetch(`https://www.prolific.ac/studies`, {
+      credentials: `include`
     });
-  }, 
-  
-  notification (obj) {
-    const noti = new Notification(obj.title, {
-      icon: `prolific.png`,
-      body: obj.body.join(`\n`)
-    });
-    
-    if (obj.timeout) {
-      setTimeout(noti.close.bind(noti), obj.timeout);
+
+    if (response.ok) {
+      const text = await response.text();
+      const studies = extractStudies(text);
+
+      resolve(studies);
+    } else {
+      reject(new Error(`${response.status} - ${response.statusText}`));
     }
-    
-    if (obj.url) {
-      noti.onclick = function () {
-        window.open(obj.url);
-        noti.close.bind(noti);
-      };
+  });
+}
+
+function notification(study) {
+  chrome.notifications.create(study.href, {
+    type: `list`,
+    title: study.title,
+    message: ``,
+    iconUrl: `/prolific.png`,
+    items: [
+      {
+        title: `Hosted By`,
+        message: study.hostedBy
+      },
+      {
+        title: `Reward`,
+        message: `${study.reward} | Avg. ${study.avgRewardPerHour}`
+      },
+      {
+        title: `Time`,
+        message: `${study.maximumAllowedTime} | Avg. ${study.avgCompletionTime}`
+      },
+      {
+        title: `Places`,
+        message: study.availablePlaces
+      }
+    ],
+    buttons: [{ title: `Open` }]
+  });
+}
+
+async function announceStudies(studies) {
+  let needToAnnounce = false;
+
+  Object.keys(studies).forEach(id => {
+    if (!notified.includes(id)) {
+      const study = studies[id];
+
+      notification(study);
+      notified.push(id);
+      needToAnnounce = true;
+    }
+  });
+
+  if (needToAnnounce) {
+    const options = await getStorage(`options`);
+
+    if (options.announce) {
+      chrome.tts.speak(`New studies availale on Prolific.`, {
+        enqueue: true,
+        voiceName: `Google US English`
+      });
     }
   }
-};
+}
 
-let options;
+function getInterval() {
+  return new Promise(async resolve => {
+    const options = await getStorage(`options`);
+    const { interval } = options;
+    const milliseconds = interval >= 60 ? interval * 1000 : 60000;
 
-chrome.storage.local.get(`options`, data => {
-  options = data.options || {
-    interval: 5,
-    announce: true
-  };
-});
+    resolve(milliseconds);
+  });
+}
 
-chrome.storage.onChanged.addListener(changes => {
-  if (changes.options) {
-    options = changes.options.newValue;
-    prolific.check();
+async function prolific() {
+  clearTimeout(timeout);
+
+  try {
+    const studies = await getStudies();
+
+    setBadge(studies);
+    setStudies(studies);
+    announceStudies(studies);
+    setChecked();
+  } catch (error) {
+    window.console.error(error);
   }
+
+  timeout = setTimeout(prolific, await getInterval());
+}
+
+chrome.storage.local.get(null, items => {
+  const { options } = items;
+
+  if (
+    !options ||
+    !Object.prototype.hasOwnProperty.call(options, `announce`) ||
+    !Object.prototype.hasOwnProperty.call(options, `interval`)
+  ) {
+    chrome.storage.local.set({ options: { announce: true, interval: 60 } });
+  }
+
+  chrome.storage.local.set({ studies: {} });
 });
 
 chrome.runtime.onMessage.addListener(request => {
-  if (request.type === `askStudies`) {
-    prolific.sendStudies();
-  }
-  if (request.type === `askBalance`) {
-    prolific.balance();
-  }
+  if (request.prolific) prolific();
 });
 
-exchange.check();
-prolific.timeout = setTimeout(prolific.check, 5000);
+chrome.notifications.onButtonClicked.addListener(notificationId => {
+  window.open(notificationId);
+  chrome.notifications.clear(notificationId);
+});
+
+timeout = setTimeout(prolific, 10000);
--- popup.js
+++ popup.js
@@ -1,116 +1,132 @@
-const popup = {
-  checked (message) {
-    document.getElementById(`checked`).innerHTML =
-      `Last Checked: ${message.checked ? message.checked.split(`(`)[0].trim() : `Never`}`
-    ;
-  }, 
-  
-  studies (message) {
-    let html = ``;
-    
-    if (Object.keys(message.studies).length === 0) {
-      html = `<div class="text-center">No Studies Available</div>`;
-    }
-    else {
-      for (let key in message.studies) {
-        const study = message.studies[key];
-        html +=
-          `<div class="card card-inverse bg-primary">
-            <h6 class="card-header">
-              <a href="https://www.prolific.ac/studies/${study.id}" target="_blank" class="text-white">${study.name}</a>
-            </h6>
-            <div class="card-block">
-              <table class="w-100">
-                <tr>
-                  <td class="w-50">
-                    <b>Hosted By:</b> ${study._researcher_name}
-                  </td>
-                  <td class="w-50">
-                    <b>Available Places:</b> ${study.places_remaining}/${study.total_available_places}
-                  </td>
-                </tr>
-                <tr>
-                  <td class="w-50">
-                    <b>Reward:</b> £${(study.reward / 10000).toFixed(2)} $${(study.reward / 10000 * message.exchangeRate).toFixed(2)}
-                  </td>
-                  <td class="w-50">
-                    <b>Time Allowed:</b> ${study.maximum_allowed_time} minutes
-                  </td>
-                </tr>
-                <tr>
-                  <td class="w-50">
-                    <b>Reward /hr:</b> £${study.average_reward_per_hour.toFixed(2)}/hr $${(study.average_reward_per_hour * message.exchangeRate).toFixed(2)}/hr
-                  </td>
-                  <td class="w-50">
-                    <b>Completion Time:</b> ${study.average_time_taken} minutes
-                  </td>
-                </tr>
-              </table>
-            </div>
-          </div>`
-        ;
-      }
-    }
-        
-    document.getElementById(`studies`).innerHTML = html;
-  },
-  
-  balance (message) {
-    document.getElementById(`balance`).innerHTML =
-      `Pend: £${message.balance.pend ? message.balance.pend.toFixed(2) : `?.??`} • Avail: £${message.balance.avail ? message.balance.avail.toFixed(2) : `?.??`}`
-    ;
-  }
-};
+function extractBalance(text) {
+  const doc = new DOMParser().parseFromString(text, `text/html`);
+  const available = doc.querySelector(
+    `[title="You must have a minimum of £5 to request a payout."`
+  );
+  const pending = doc.querySelector(
+    `[title="This is the amount of money that you may earn if responses awaiting reviews are approved."]`
+  );
 
-document.addEventListener(`DOMContentLoaded`, event => {
-  chrome.storage.local.get(`options`, data => {
-    if (data.options) {
-      const interval = document.getElementById(`interval`);
-      const announce = document.getElementById(`announce`);
-      
-      interval.value = data.options.interval;
-      announce.checked = data.options.announce;
-      
-      if (data.options.announce === false) {
-        announce.parentElement.className = announce.parentElement.className.replace(`btn-primary`, `btn-secondary`);
+  return {
+    available: available.textContent.match(/([0-9.]+)/)[0],
+    pending: pending.textContent.match(/([0-9.]+)/)[0]
+  };
+}
+
+function getBalance() {
+  return new Promise(async (resolve, reject) => {
+    const response = await fetch(
+      `https://www.prolific.ac/participant/account`,
+      {
+        credentials: `include`
       }
+    );
+
+    if (response.url.includes(`https://www.prolific.ac/login`)) {
+      reject(new Error(`Logged Out!`));
+    } else if (response.ok) {
+      const text = await response.text();
+      const balance = extractBalance(text);
+
+      resolve(balance);
+    } else {
+      reject(new Error(`${response.status} - ${response.statusText}`));
     }
   });
-  
-  chrome.runtime.onMessage.addListener(request => {
-    if (request.type === `sendStudies`) {
-      popup.checked(request.message);
-      popup.studies(request.message);
-    }
-    if (request.type === `balance`) {
-      popup.balance(request.message);
-    }
-  });
-  
-  chrome.runtime.sendMessage({
-    type: `askStudies`
-  });
-  
-  chrome.runtime.sendMessage({
-    type: `askBalance`
-  });
+}
+
+async function displayBalance() {
+  const balanceEl = document.getElementById(`balance`);
+
+  try {
+    const balance = await getBalance();
+    const { available, pending } = balance;
+
+    balanceEl.textContent = `Pend: £${pending} • Avail: £${available}`;
+  } catch (error) {
+    balanceEl.textContent = error;
+  }
+}
+
+function displayChecked(checked) {
+  document.getElementById(`checked`).textContent = `Last Checked: ${checked}`;
+}
+
+function displayOptions(options) {
+  document.getElementById(`announce`).checked = options.announce;
+  document.getElementById(`interval`).value = options.interval;
+}
+
+function studyHTML(study) {
+  const { href, title } = study;
+
+  return `<div class="card mt-2">
+    <div class="card-header p-0" style="line-height: 1;">
+      <b><a href="${href}" target="_blank">${title}</a></b>
+    </div>
+    <div class="card-block" style="line-height: 1.25;">
+      <table class="w-100 small">
+        <tr>
+          <td class="w-50">
+            <b>Hosted By:</b> <span>${study.hostedBy}</span>
+          </td>
+          <td class="w-50">
+            <b>Available Places:</b> <span>${study.availablePlaces}</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="w-50">
+            <b>Reward:</b> <span>${study.reward}</span>
+          </td>
+          <td class="w-50">
+            <b>Time Allowed:</b> <span>${study.maximumAllowedTime}</span>
+          </td>
+        </tr>
+        <tr>
+          <td class="w-50">
+            <b>Reward /hr:</b> <span>${study.avgRewardPerHour}</span>
+          </td>
+          <td class="w-50">
+            <b>Completion Time:</b> <span>${study.avgCompletionTime}</span>
+          </td>
+        </tr>
+      </table>
+    </div>
+  </div>`;
+}
+
+function displayStudies(studies) {
+  const ids = Object.keys(studies);
+
+  if (ids.length) {
+    Object.keys(studies).forEach(id => {
+      document
+        .getElementById(`studies`)
+        .insertAdjacentHTML(`beforeend`, studyHTML(studies[id]));
+    });
+  } else {
+    document
+      .getElementById(`studies`)
+      .insertAdjacentHTML(
+        `beforeend`,
+        `<div class="text-center">No Studies</div>`
+      );
+  }
+}
+
+chrome.storage.local.get(null, items => {
+  const { checked, options, studies } = items;
+
+  displayBalance();
+  displayChecked(checked);
+  displayOptions(options);
+  displayStudies(studies);
 });
 
-document.addEventListener(`change`, event => {
-  const interval = document.getElementById(`interval`);
-  const announce = document.getElementById(`announce`);
-    
-  if (announce.checked === true && announce.parentElement.className.match(`btn-secondary`)) {
-    announce.parentElement.className = announce.parentElement.className.replace(`btn-secondary`, `btn-primary`);
-  }
-  else if (announce.checked === false && announce.parentElement.className.match(`btn-primary`)) {
-    announce.parentElement.className = announce.parentElement.className.replace(`btn-primary`, `btn-secondary`);
-  }
-  
-  chrome.storage.local.set({
-    options: {
-      interval: interval.value,
-      announce: announce.checked
-    }
-  });
+document.addEventListener(`change`, () => {
+  const interval = document.getElementById(`interval`).value;
+  const announce = document.getElementById(`announce`).checked;
+
+  chrome.storage.local.set({ options: { announce, interval } });
+  chrome.runtime.sendMessage({ prolific: true });
 });
