eab111c977b3e0cfc45b8997dadd556b7250affc
kadauchi <kadauchi@gmail.com>
2018-08-14 21:44:48 -0700

    Try to display institution name first. Removed time allowed section.

--- popup.js
+++ popup.js
@@ -1,61 +1,5 @@
 const toMoney = (n) => (n / 100).toFixed(2);
 
-/*
-function extractBalance(text) {
-  const doc = new DOMParser().parseFromString(text, `text/html`);
-  const available = doc.querySelector(
-    `[title="You must have a minimum of £5 to request a payout."`
-  );
-  const pending = doc.querySelector(
-    `[title="This is the amount of money that you may earn if responses awaiting reviews are approved."]`
-  );
-
-  return {
-    available: available.textContent.match(/([0-9.]+)/)[0],
-    pending: pending.textContent.match(/([0-9.]+)/)[0]
-  };
-}
-*/
-
-/*
-function getBalance() {
-  return new Promise(async (resolve, reject) => {
-    const response = await fetch(
-      `https://www.prolific.ac/participant/account`,
-      {
-        credentials: `include`
-      }
-    );
-
-    if (response.url.includes(`https://www.prolific.ac/login`)) {
-      reject(new Error(`Logged Out!`));
-    } else if (response.ok) {
-      const text = await response.text();
-      const balance = extractBalance(text);
-
-      resolve(balance);
-    } else {
-      reject(new Error(`${response.status} - ${response.statusText}`));
-    }
-  });
-}
-*/
-
-/*
-async function displayBalance() {
-  const balanceEl = document.getElementById(`balance`);
-
-  try {
-    const balance = await getBalance();
-    const { available, pending } = balance;
-
-    balanceEl.textContent = `Pend: £${pending} • Avail: £${available}`;
-  } catch (error) {
-    balanceEl.textContent = error;
-  }
-}
-*/
-
 function displayChecked(checked) {
   document.getElementById(`checked`).textContent = `Last Checked: ${checked}`;
 }
@@ -74,7 +18,7 @@ function studyHTML(study) {
       <table class="w-100 small">
         <tr>
           <td class="w-50">
-            <b>Hosted By:</b> <span>${study.researcher.name}</span>
+            <b>Hosted By:</b> <span>${study.researcher.institution.name || study.researcher.name}</span>
           </td>
           <td class="w-50">
             <b>Available Places:</b> <span>${study.total_available_places - study.places_taken}</span>
@@ -85,13 +29,10 @@ function studyHTML(study) {
             <b>Reward:</b> <span>${toMoney(study.reward)}</span>
           </td>
           <td class="w-50">
-            <b>Time Allowed:</b> <span>N/A</span>
+            <b>Reward /hr:</b> <span>${toMoney(study.average_reward_per_hour)}</span>
           </td>
         </tr>
         <tr>
-          <td class="w-50">
-            <b>Reward /hr:</b> <span>${toMoney(study.average_reward_per_hour)}</span>
-          </td>
           <td class="w-50">
             <b>Completion Time:</b> <span>${study.estimated_completion_time} minutes</span>
           </td>
