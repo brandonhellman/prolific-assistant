f4fdf1687df357351fef9b4ea1a303c7805ae02c
kadauchi <kadauchi@gmail.com>
2018-08-14 21:45:48 -0700

    Listens for all prolific api requests.
    Saves the headers to use for future requests if the headers contain 'Authorization'.

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
+let headers;
 let timeout;
 const notified = [];
 
-const toMoney = (n) => (n / 100).toFixed(2);
+const toMoney = n => (n / 100).toFixed(2);
 
 function setBadge(studies) {
   const count = Object.keys(studies).length;
@@ -34,9 +35,12 @@ function getStorage(key) {
 
 function getStudies() {
   return new Promise(async (resolve, reject) => {
-    const response = await fetch(`https://www.prolific.ac/api/v1/studies/?current=1`, {
-      credentials: `include`
-    });
+    const response = await fetch(
+      `https://www.prolific.ac/api/v1/studies/?current=1`,
+      {
+        credentials: `include`
+      }
+    );
 
     if (response.ok) {
       const json = await response.json();
@@ -48,7 +52,6 @@ function getStudies() {
 }
 
 function notification(study) {
-  
   chrome.notifications.create(study.id, {
     type: `list`,
     title: study.name,
@@ -61,7 +64,9 @@ function notification(study) {
       },
       {
         title: `Reward`,
-        message: `${toMoney(study.reward)} | Avg. ${toMoney(study.average_reward_per_hour)}`
+        message: `${toMoney(study.reward)} | Avg. ${toMoney(
+          study.average_reward_per_hour
+        )}`
       },
       {
         title: `Places`,
@@ -70,14 +75,16 @@ function notification(study) {
     ],
     buttons: [{ title: `Open` }]
   });
-  
 }
 
 async function announceStudies(studies) {
-  const needAnnouncing = studies.filter((o) => !notified.includes(o.id));
+  const needAnnouncing = studies.filter(o => !notified.includes(o.id));
 
   if (needAnnouncing.length) {
-    needAnnouncing.forEach((o) => { notified.push(o.id); notification(o); })
+    needAnnouncing.forEach(o => {
+      notified.push(o.id);
+      notification(o);
+    });
 
     const options = await getStorage(`options`);
 
@@ -105,7 +112,6 @@ async function prolific() {
 
   try {
     const studies = await getStudies();
-
     setBadge(studies);
     setStudies(studies);
     announceStudies(studies);
@@ -139,5 +145,23 @@ chrome.notifications.onButtonClicked.addListener(notificationId => {
   window.open(`https://app.prolific.ac/studies/${notificationId}`);
   chrome.notifications.clear(notificationId);
 });
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    const { requestHeaders } = details;
+    const authorizationHeaderExists = requestHeaders.some(
+      header => header.name === "Authorization"
+    );
+
+    if (authorizationHeaderExists) {
+      headers = requestHeaders;
+    } else if (headers) {
+      return { requestHeaders: headers };
+    }
+  },
+  { urls: ["https://www.prolific.ac/api/v1/studies*"] },
+  ["blocking", "requestHeaders"]
+);
+
 
 prolific();
