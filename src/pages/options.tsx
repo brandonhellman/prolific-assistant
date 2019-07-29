f7b6e1b116046490d78c45a9140835999bf08327
Kadauchi <kadauchi@gmail.com>
2019-07-29 12:35:36 -0700

    Removed options page.

--- src/pages/options.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-
-import { Hello } from '../components/Hello';
-
-ReactDom.render(<Hello from="options.tsx" />, document.getElementById('root'));
