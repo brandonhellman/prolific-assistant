97125e1637c716fd7f8360e2d30e74e094faaa02
Kadauchi <kadauchi@gmail.com>
2019-08-11 15:04:10 -0700

    Added redux-logger.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { applyMiddleware, createStore, combineReducers, Middleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { wrapStore } from 'webext-redux';
@@ -6,6 +7,8 @@ import { wrapStore } from 'webext-redux';
 import { prolificReducer } from './prolific/reducers';
 import { sessionReducer } from './session/reducers';
 import { settingsReducer } from './settings/reducers';
+
+const logger = createLogger();
 
 const persistConfig = {
   key: 'settings',
@@ -25,7 +28,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export type AppState = ReturnType<typeof rootReducer>;
 
 export function configureStore(...middlewares: Middleware[]) {
-  const store = createStore(persistedReducer, applyMiddleware(...middlewares));
+  const store = createStore(persistedReducer, applyMiddleware(...middlewares, logger));
   persistStore(store);
   wrapStore(store);
   return store;
