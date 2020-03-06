aa51d5d8094c9f9e67eef5ff5e4b0895fc38fed1
coxy <andrew@prolific.ac>
2020-03-06 11:57:52 +0000

    feat: support both oauth and jwt auth strategies

--- src/functions/fetchProlificStudies.ts
+++ src/functions/fetchProlificStudies.ts
@@ -1,5 +1,8 @@
-export async function fetchProlificStudies() {
-  const response = await fetch('https://www.prolific.co/api/v1/studies/?current=1', { credentials: 'include' });
+export async function fetchProlificStudies(authHeader: any) {
+  const { name, value } = authHeader
+  const headers = { [name]: value }
+  // omit credentials here, since auth is handled via the bearer token
+  const response = await fetch('https://www.prolific.co/api/v1/studies/?current=1', { credentials: 'omit', headers });
   const json: ProlificApiStudies = await response.json();
   return json;
 }
--- src/pages/background.ts
+++ src/pages/background.ts
@@ -1,4 +1,4 @@
-import { browser, Tabs, WebRequest } from 'webextension-scripts/polyfill';
+import { browser, WebRequest } from 'webextension-scripts/polyfill';
 
 import { fetchProlificStudies } from '../functions/fetchProlificStudies';
 import { openProlificStudy } from '../functions/openProlificStudy';
@@ -11,12 +11,12 @@ import { settingsAlertSoundMiddleware } from '../store/settingsAlertSoundMiddlew
 const store = configureStore(prolificStudiesUpdateMiddleware, settingsAlertSoundMiddleware);
 
 let authHeader: WebRequest.HttpHeadersItemType;
-let authTab: Tabs.Tab & { loggedOut?: boolean };
 let timeout = window.setTimeout(main);
 
-async function openAuthTab() {
-  authHeader = null;
-  authTab = await browser.tabs.create({ url: 'https://app.prolific.co/', active: false });
+function updateResults(results: any[]) {
+  store.dispatch(prolificStudiesUpdate(results));
+  store.dispatch(sessionLastChecked());
+  browser.browserAction.setBadgeText({ text: results.length ? results.length.toString() : '' });
 }
 
 async function main() {
@@ -25,21 +25,18 @@ async function main() {
 
   if (authHeader) {
     try {
-      const response = await fetchProlificStudies();
+      const response = await fetchProlificStudies(authHeader);
 
       if (response.results) {
-        store.dispatch(prolificStudiesUpdate(response.results));
-        store.dispatch(sessionLastChecked());
-        browser.browserAction.setBadgeText({ text: response.results.length ? response.results.length.toString() : '' });
+        updateResults(response.results)
         browser.browserAction.setBadgeBackgroundColor({ color: 'red' });
       }
 
       if (response.error) {
         if (response.error.status === 401) {
-          store.dispatch(prolificErrorUpdate(response.error.status));
+          store.dispatch(prolificErrorUpdate(401));
           browser.browserAction.setBadgeText({ text: '!' });
           browser.browserAction.setBadgeBackgroundColor({ color: 'red' });
-          openAuthTab();
         } else {
           store.dispatch(prolificStudiesUpdate([]));
           browser.browserAction.setBadgeText({ text: 'ERR' });
@@ -52,8 +49,9 @@ async function main() {
       browser.browserAction.setBadgeBackgroundColor({ color: 'black' });
       window.console.error('fetchProlificStudies error', error);
     }
-  } else if (!authTab) {
-    openAuthTab();
+  } else {
+    // openAuthTab();
+    store.dispatch(prolificErrorUpdate(401));
   }
 
   timeout = window.setTimeout(main, state.settings.check_interval * 1000);
@@ -64,33 +62,48 @@ browser.notifications.onClicked.addListener((notificationId) => {
   openProlificStudy(notificationId);
 });
 
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (authTab && authTab.id == tabId && !authTab.loggedOut) {
-    if (changeInfo.status == 'complete') {
-      if (tab.url === 'https://app.prolific.co/login') {
-        authTab.loggedOut = true;
-        browser.tabs.highlight({ windowId: tab.windowId, tabs: tab.index });
-      }
-    }
-  }
-});
+function handleSignedOut() {
+  authHeader = null
+  updateResults([])
+  store.dispatch(prolificErrorUpdate(401))
+}
+
+// Watch for url changes and handle sign out
+browser.webNavigation.onCompleted.addListener(
+  (details) => {
+    handleSignedOut();
+  },
+  {
+    url: [{ urlEquals: 'https://www.prolific.co/auth/accounts/login/'}],
+  },
+);
+
+// Prolific is a single page app, so we need to watch
+// the history state for changes too
+browser.webNavigation.onHistoryStateUpdated.addListener(
+  (details) => {
+    handleSignedOut();
+  },
+  {
+    url: [{ urlEquals: 'https://app.prolific.co/login'}],
+  },
+);
 
 // Parse and save the Authorization header from any Prolific request.
 browser.webRequest.onBeforeSendHeaders.addListener(
   (details) => {
     const foundAuthHeader = details.requestHeaders.find((header) => header.name === 'Authorization');
 
+
     if (foundAuthHeader) {
+      if (foundAuthHeader.value === 'Bearer null') {
+        return
+      }
+
       let restart = false;
 
       if (!authHeader) {
         restart = true;
-      }
-
-      if (authTab && authTab.id === details.tabId && !authTab.loggedOut) {
-        restart = true;
-        authTab = null;
-        browser.tabs.remove(details.tabId);
       }
 
       authHeader = foundAuthHeader;
@@ -103,24 +116,7 @@ browser.webRequest.onBeforeSendHeaders.addListener(
     return {};
   },
   {
-    urls: ['https://*.prolific.co/*'],
-  },
-  ['blocking', 'requestHeaders'],
-);
-
-// Add the auth header to any reqeuest from the background to the studies API.
-browser.webRequest.onBeforeSendHeaders.addListener(
-  (details) => {
-    if (authHeader && details.tabId === -1 && details.method === 'GET') {
-      return {
-        requestHeaders: [...details.requestHeaders, authHeader],
-      };
-    }
-
-    return {};
-  },
-  {
-    urls: ['https://www.prolific.co/api/v1/studies/*'],
+    urls: ['https://www.prolific.co/api/*'],
   },
   ['blocking', 'requestHeaders'],
 );
